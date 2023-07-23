b673f8f74c7e55ac9d81ace9874cd854687bb998
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-23 15:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function highlight() {
     //Write your code here
 
+    const strong = document.getElementsByTagName('strong');
+   
+    for(let i=0; i<strong.length; i++)
+    {
+        strong[i].style.color ="green";
+    }
 
+   
 }
 
 
 function return_normal() {
     //Write your code here
-
+    const strong = document.getElementsByTagName('strong');
+   
+    for(let i=0; i<strong.length; i++)
+    {
+        strong[i].style.color ="black";
+    }
     
 }
+
